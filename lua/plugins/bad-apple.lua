016dabcd6d35c9eb3a92c2568fc843d905bdab42
vgoer <goerblue@gmail.com>
2024-10-11 10:21:46 +0800

    add: nvim bad-apple

---
+++ lua/plugins/bad-apple.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "seandewar/bad-apple.nvim",
+    config = function()
+      -- 可选配置，例如设置 libcanberra 的路径
+      -- require("bad-apple.sound").libcanberra_fname = "/path/to/libcanberra"
+    end,
+  },
+}
